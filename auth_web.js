56b70a40a94bb8c9f1a8de40e0af0c94663e1ce9
leoyuholo <leoyuholo@gmail.com>
2014-05-30 17:33:08 +0800

    added login and logout, also a little bit refactoring

--- auth_web.js
+++ auth_web.js
@@ -1,15 +1,28 @@
+var urlAjax = function(url, onSuccess, onFail) {
+	$.ajax({
+		url: url
+	}).done(function(res) {
+		if (res.result) {
+			if (onSuccess)
+				onSuccess(res.payload)
+		} else {
+			if (onFail)
+				onFail(res.payload);
+		}
+	});
+}
+
 auth = {
+
 	create: function(id, pw, onSuccess, onFail) {
-		$.ajax({
-			url: '/create/' + id + '/' + pw
-		}).done(function(res) {
-			if (res.result) {
-				if (onSuccess)
-					onSuccess(res.payload)
-			} else {
-				if (onFail)
-					onFail(res.payload);
-			}
-		});
+		urlAjax('/create/' + id + '/' + pw, onSuccess, onFail);
+	},
+
+	login: function(id, pw, onSuccess, onFail) {
+		urlAjax('/login/' + id + '/' + pw, onSuccess, onFail);
+	},
+
+	logout: function(id, token, onSuccess, onFail) {
+		urlAjax('/logout/' + id + '/' + token, onSuccess, onFail);
 	}
 }
